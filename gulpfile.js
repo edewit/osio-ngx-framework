46d03ee08f794b65deead93865caa2c4d334989d
Michael Kleinhenz <kleinhenz@redhat.com>
2017-09-06 09:58:30 +0200

    fix(build): unified the tsconfig for build and demo.

--- gulpfile.js
+++ gulpfile.js
@@ -115,7 +115,7 @@ gulp.task('build',
   ]);
 
 gulp.task('transpile', ['copy-root', 'inline-template'], function () {
-  return ngc('tsconfig-library.json');
+  return ngc('tsconfig.json');
 });
 
 gulp.task('inline-template', ['transpile-less'], function () {
