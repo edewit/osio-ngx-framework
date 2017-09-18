5d57b0c7d349717468fa9589e56f523fb7619265
Michael Kleinhenz <kleinhenz@redhat.com>
2017-09-18 17:52:14 +0200

    fix(exports): Fixed exports.

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 export { OSIONGXFrameworkModule } from './osio-ngx-framework.module';
 
+// Framework Services
+export { FrameworkServicesModule } from './src/app/framework-services/framework-services.module';
+export { AboutService } from './src/app/framework-services/about.service';
+
 // Header
 export { HeaderModule } from './src/app/header/header.module';
-export { HeaderComponent} from './src/app/header/header.component';
-export { HeaderService} from './src/app/header/header.service';
+export { HeaderComponent } from './src/app/header/header.component';
+export { HeaderService } from './src/app/header/header.service';
+export { MenuItem } from './src/app/header/menu-item';
+export { MenuedContextType } from './src/app/header/menued-context-type';
+export { SystemStatus } from './src/app/header/system-status';
 
 // Footer
 export { FooterModule } from './src/app/footer/footer.module';
